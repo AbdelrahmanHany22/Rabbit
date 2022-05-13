63325e3e0aef64031f273dbad80be430a3e58bd4
AbdelrahmanHany22 <boudyhany22@gmail.com>
2022-05-13 17:42:44 +0200

    removed from main

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config({path:'../Config.env'});
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-module.exports = app;
