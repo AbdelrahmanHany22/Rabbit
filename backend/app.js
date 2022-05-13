a246bfd19cac0f4b230619342c56de0369acec71
AbdelrahmanHany22 <boudyhany22@gmail.com>
2022-05-13 18:54:58 +0200

    Added configs

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config({path:'../Config.env'});
 const app = express();
+const mongoose = require('mongoose');
+
+const uri = process.env.DATABASE_STRING;
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+console.log("MongoDB database connection established successfully");
+})
+
 
 app.use(cors());
 app.use(express.json());
