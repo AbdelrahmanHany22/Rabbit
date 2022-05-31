fa1b3125fdadd11fdaa64d4d795cd5d971021461
Iyad Elwy <iyadelwy1@gmail.com>
2022-05-31 21:32:41 +0200

    added frontend and payment and notification

--- backend/Payment-API/routes/stripeRoutes.js
+++ backend/Payment-API/routes/stripeRoutes.js
@@ -1,35 +1,35 @@
 const express = require('express');
 
 const stripe = require('stripe')(process.env.Private_Api_key);
-const {v4:uuidv4} = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
 const router = express.Router();
 
-router.get('/',(req,res,next) =>{
+router.get('/', (req, res, next) => {
     console.log("Get Response from researcher");
     res.json({
         message: 'IT WORKS!'
     });
 });
 
-router.post("/pay", (req,res,next) =>{
+router.post("/pay", (req, res, next) => {
     console.log(req.body.token);
-    const idempotencykey= uuidv4();
+    const idempotencykey = uuidv4();
 
     return stripe.customers.create({
         email: token.email,
         source: token
-    }).then(customer=>{
+    }).then(customer => {
         stripe.charges.create({
-            amount: amount *100,
+            amount: amount * 100,
             currency: 'egp',
             customer: customer.id,
             recipt_email: token.email
-        },{idempotencykey})
-    }).then(result =>{
-        res.status(200).json(result)
-    }).catch(err =>{
-        console.log(err);
+        }, {idempotencykey});
+    }).then(result => {
+        res.status(200).json(result);
+    }).catch(err => {
+        res.status(500).json(err);
     });
 });
 
