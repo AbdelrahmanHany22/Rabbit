8a0ef22ad6a9e649264a30a680456ed02d363c7b
Hini <youssef.elhini@gmail.com>
2022-06-04 13:25:50 +0200

    worked on dashboad predictions controller

--- AdminDashboard-API/controllers/PredictionControllers.js
+++ AdminDashboard-API/controllers/PredictionControllers.js
@@ -1,16 +1,22 @@
 const Prediction = require('../models/SingleOrderProducts');
 const generalController = require('./generalControllers')
 const catchAsync = require("../utils/catchAsync");
-
+const axios = require("axios").default;
+const AppError = require('../utils/appError');
 
 
 /////////////////////////////////////////////////////////////
 
 exports.createPrediction = catchAsync(async (req, res, next) => {
-
+    let predictions;
+    try {
+        predictions = axios.get("http://127.0.0.1:8000/productassociations");
+    } catch (error) {
+        next(new AppError('Error occured while making predictions.',500));
+    }
     res.status(201).json({
         status: "Success",
-        data: doc,
+        data: predictions,
     });
 
 });
